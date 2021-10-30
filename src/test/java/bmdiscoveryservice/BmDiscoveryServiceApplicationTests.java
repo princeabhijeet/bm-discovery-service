a364c995c2e63ace25dd7e0914d4123ef5cd11c3
princeabhijeet <princeabhijeet1994@gmail.com>
2021-10-30 12:27:10 +0530

    develop - first code commit

---
+++ src/test/java/bmdiscoveryservice/BmDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bmdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
